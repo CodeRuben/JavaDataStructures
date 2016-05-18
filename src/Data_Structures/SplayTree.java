0541cc437fdc981b7b4108b4671cb837bfc8d96f
Ruben Ramirez <ruben1991@att.net>
2016-05-17 21:38:29 -0700

    Second commit

--- src/Data_Structures/SplayTree.java
+++ src/Data_Structures/SplayTree.java
@@ -1,5 +1,12 @@
-
 package Data_Structures;
+
+/**
+ * Implementation of a generic top-down splay tree
+ * Items are added as key-value pairs into the tree
+ * @author Ruben Ramirez
+ * @param <K>, keys stored in the tree
+ * @param <V>, values stored in the tree
+ */
 
 public class SplayTree<K,V> {
     public Node<K,V> root;
@@ -10,169 +17,217 @@ public class SplayTree<K,V> {
         this.currentSize = 0;
     }
     
-    public boolean add(K key, V val) {
-        Node<K,V> newNode = new Node<>(key, val);
+    /**
+     * Adds an entry into the splay tree. Duplicate entries override existing 
+     * data.
+     * @param obj, the item to be added to the tree
+     * @return true if the item was successfully added to the tree
+     */
+    public boolean add(K key, V value) {
+        Node<K,V> newNode = new Node<>(key, value);
         
         if(root == null) {
             root = newNode;
-            currentSize++;
+            this.currentSize++;
             return true;
         }
-        if(addNode(root, newNode) == false)
-            return true;
-        currentSize++;
+        splay(newNode);
+        
+        if(((Comparable<K>)key).compareTo(root.key) == 0) 
+            return false;
+        
+        if(((Comparable<K>)key).compareTo(root.key) < 0) {
+            newNode.left = root.left;
+            newNode.right = root;
+            root.left = null;
+            root = newNode;
+        }
+        else {
+            newNode.right = root.right;
+            newNode.left = root;
+            root.right = null;
+            root = newNode;
+        }
+        this.currentSize++;
         return true;
     }
-    
-    private boolean addNode(Node<K,V> parent, Node<K,V> newNode) {
-        
-        if(((Comparable<K>)newNode.key).compareTo(parent.key) < 0) {
-            if(parent.left == null) {
-                parent.left = newNode;
-                newNode.parent = parent;
-                splay(newNode);
-                return true;
-            }
-            else
-                addNode(parent.left, newNode);
-        }
-        else if(((Comparable<K>)newNode.key).compareTo(parent.key) > 0) {
-            if(parent.right == null) {
-                parent.right = newNode;
-                newNode.parent = parent;
-                splay(newNode);
-                return true;
-            }
-            else
-                addNode(parent.right, newNode);
-        }
-        else
-            parent.value = newNode.value;
-        return false;
-    }
-    
+    /**
+     * Searches the tree to see if an item exists in the structure
+     * @param obj, the object to be searched for
+     * @return true if the object is found, false otherwise
+     */
     public boolean contains(K key) {
-        if(isEmpty())
-            return false;
-        return contains(null, root, key);
-    }
-    
-    public boolean isEmpty() {
-        return currentSize == 0;
-    }
-    
-    public boolean isFull() {
+        Node<K,V> node = new Node<>(key, null);
+        
+        splay(node);
+        if(((Comparable<K>)key).compareTo(root.key) == 0) 
+            return true;
         return false;
     }
     
-    private boolean contains(Node<K,V> top, Node<K,V> node, K toFind) {
-        if(node == null) {
-            if(top == root)
-                return false;
-            splay(top);
-            return false;
+    /**
+     * Returns the value of the element stored at the root of the tree
+     * @return the value stored in the root node
+     */
+    public V peek() {
+        if(isEmpty())
+            return null;
+        return root.value;
+    }
+    
+    public K remove(K key) {
+        Node<K,V> node = new Node<>(key, null);
+        Node<K,V> tmp; 
+        if(root == null)
+            return null;
+        
+        splay(node);
+        
+        // If object is not in the tree
+        if(((Comparable<K>)key).compareTo(root.key) != 0) 
+            return null;
+        
+        // If largest element has to be deleted
+        if(root.right == null) {
+            root = root.left;
+            currentSize--;
+            return key;
         }
-
-        if(((Comparable<K>)toFind).compareTo(node.key) < 0)
-            return contains(node, node.left, toFind);
-        else if(((Comparable<K>)toFind).compareTo(node.key) > 0)
-            return contains(node, node.right, toFind);
-        else {
-            if(node != root)
-                splay(node);
-            return true;
+        // Get the in order successor and delete it
+        tmp = deleteNext(root);
+        root.key = tmp.key;
+        return key;
+    }
+    
+    /**
+     * Helped method that finds the in order successor of a given node and 
+     * deletes it. Returns the value of the in order successor to be placed
+     * at the root of the tree.
+     * @param node, node to be used to find the successor node
+     * @return the node containing the in order successor, null if successor
+     * doesn't exist
+     */
+    private Node<K,V> deleteNext(Node<K,V> node) {
+        Node<K,V> tmp = null;
+        if(node == null)
+            return null;
+        
+        if(node.right != null) {
+            if(node.right.left == null) {
+                tmp = node.right;
+                node.right = tmp.right;
+                currentSize--;
+                return tmp;
+            }
+            
+            node = node.right;
+            tmp = node;
+            while(node.left != null) {
+                tmp = node;
+                node = node.left;
+            }
+            
+            // Node has no child nodes
+            if(node.right == null && tmp != null) {
+                tmp.left = null;
+                currentSize--;
+                return node;
+            }
+            else {
+                tmp.left = node.right;
+                currentSize--;
+                return node;
+            }
         }
+        return null;
     }
     
     private void splay(Node<K,V> node) {
-        Node<K,V> parent = node.parent;
-        Node<K,V> grand = node.parent.parent;
+        Node<K,V> holder = new Node<>(null, null);
+        Node<K,V> L, R, top;
+        top = root;
+        L = R = holder;
         
-        while(node != root) {
-            if(parent == root) {
-                //single rotate on root
-                if(((Comparable<K>)node.key).compareTo(node.parent.key) < 0)
-                    rightRotate(root);
-                else
-                    leftRotate(root);
-                root.parent = null;
-                return;
-            }
-            if(((Comparable<K>)parent.key).compareTo(parent.parent.key) > 0) {
-                //right/left(right-left rotate)
-                if(((Comparable<K>)node.key).compareTo(node.parent.key) < 0) {
-                    rightRotate(parent);
-                    leftRotate(grand);
+        while(true) {
+            if(((Comparable<K>)node.key).compareTo(top.key) < 0) {
+                if(top.left == null) 
+                        break;
+                if(((Comparable<K>)node.key).compareTo(top.left.key) < 0) {
+                    top = rightRotate(top);
+                    if(top.left == null) 
+                        break;
                 }
-                //right/right(left-left rotate)
-                else {
-                    leftRotate(grand);
-                    leftRotate(parent);
-                }
+                R.left = top;
+                R = top;
+                top = R.left;
+                R.left = null;
             }
-            else {
-                //left/right(left-right rotate)
-                if(((Comparable<K>)node.key).compareTo(node.parent.key) > 0) {
-                    leftRotate(parent);
-                    rightRotate(grand);
+            else if(((Comparable<K>)node.key).compareTo(top.key) > 0) {
+                if(top.right == null) 
+                        break;
+                if(((Comparable<K>)node.key).compareTo(top.right.key) > 0) {
+                    top = leftRotate(top);
+                    if(top.right == null) 
+                        break;
                 }
-                //left/left(right-right rotate)
-                else { 
-                    rightRotate(grand);
-                    rightRotate(parent);
-                }
+                L.right = top;
+                L = top;
+                top = L.right;
+                L.right = null;
             }
-            if(node.parent != null) {
-                parent = node.parent;
-                grand = node.parent.parent;
-            }
+            else
+                break;
         }
+        L.right = top.left;
+        R.left = top.right;
+        top.left = holder.right;
+        top.right = holder.left;
+        root = top;
     }
     
-    private void leftRotate(Node<K,V> node) {
-        Node<K,V> newTop = node.right;
-        node.right = newTop.left;
-        if(newTop.left != null)
-            newTop.left.parent = node;
-        
-        if(node.parent == null)
-            root = newTop;
-        else if(((Comparable<K>)node.key).compareTo(node.parent.key) <= 0)
-            node.parent.left = newTop;
-        else
-            node.parent.right = newTop;
-        
-        newTop.left = node;
-        newTop.parent = node.parent;
-        node.parent = newTop;
+    /**
+     * Returns the number of elements stored in the tree
+     * @return int, the amount of nodes within the tree
+     */
+    public int size() {
+        return this.currentSize;
     }
     
-    private void rightRotate(Node<K,V> node) {
+    /**
+     * Checks to see if the tree is currently empty
+     * @return true if there are no elements in the tree, false otherwise
+     */
+    public boolean isEmpty() {
+        return this.currentSize == 0;
+    }
+    
+    /**
+     * Performs a right rotation on a given node. 
+     * @param node, the node to be rotated
+     * @return 
+     */
+    private Node<K,V> rightRotate(Node<K,V> node) {
         Node<K,V> newTop = node.left;
         node.left = newTop.right;
-        if(newTop.right != null)
-            newTop.right.parent = node;
-        
-        if(node.parent == null)
-            root = newTop;
-        else if(((Comparable<K>)node.key).compareTo(node.parent.key) <= 0)
-            node.parent.left = newTop;
-        else
-            node.parent.right = newTop;
-        
         newTop.right = node;
-        newTop.parent = node.parent;
-        node.parent = newTop;
+        return newTop;
     }
     
-    public void inorder(Node<K,V> node) {
+    private Node<K,V> leftRotate(Node<K,V> node) {
+        Node<K,V> newTop = node.right;
+        node.right = newTop.left;
+        newTop.left = node;
+        return newTop;
+    }
+    
+    public void inOrder(Node<K,V> node) {
         if(node != null) {
-            inorder(node.left);
+            inOrder(node.left);
             System.out.print(node.key + " ");
-            inorder(node.right);
+            inOrder(node.right);
         }
     }
-    
+
     public void breadthTraversal() {
         Queue<Node<K,V>> q = new Queue<>();
         Node<K,V> node;
@@ -197,14 +252,13 @@ public class SplayTree<K,V> {
     private class Node<K,V> implements Comparable<Node<K,V>> {
         private K key;
         private V value;
-        private Node<K,V> left, right, parent;
+        private Node<K,V> left, right;
         
         public Node(K key, V val) {
             this.key = key;
             this.value = val;
             this.left = null;
             this.right = null;
-            this.parent = null;
         }
         
         public int compareTo(Node<K,V> node) {
--- src/javadatastructures/JavaDataStructures.java
+++ src/javadatastructures/JavaDataStructures.java
@@ -8,14 +8,13 @@ public class JavaDataStructures {
     public static void main(String[] args) {
         SplayTree<Integer, Integer> splayTree = new SplayTree<>();
 
-        for(int i = 0; i < 10000; i++) {
-            splayTree.add(i + 1, i  + 1);
-            splayTree.add(i / 2, i  + 1);
-            splayTree.add(i - 2, i  + 1);
-            splayTree.add(i << 3, i  + 1);  
+        int random = 0;
+        for(int i = 0; i < 100; i++) {
+            splayTree.add(i, i); 
         }
         
-        System.out.println(splayTree.contains(60769));
+        splayTree.contains(50);
+        //System.out.println(splayTree.contains(1));
         //splayTree.inorder(splayTree.root);
         System.out.println();
         splayTree.breadthTraversal();
